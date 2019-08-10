b6aee76de751ee0a6e31694a77ffe871252c9340
jiajie <jiajie.wu96@gmail.com>
2019-08-10 16:53:29 -0700

    Added movie url method to network utils

--- app/src/main/java/com/example/popularmovies/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/popularmovies/Utils/NetworkUtils.java
@@ -1,5 +1,10 @@
 package com.example.popularmovies.Utils;
 
+import android.net.Uri;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class NetworkUtils {
     private static final String TAG = NetworkUtils.class.getSimpleName();
 
@@ -11,6 +16,19 @@ public class NetworkUtils {
 
     private static String POPULAR_PARAM = "popular";
     private static String TOP_RATED_PARAM = "top_rated";
+    private static String API_KEY_PARAM = "api_key";
 
-
+    public static URL buildMovieUrl(String sortParam){
+        Uri builtUri = Uri.parse(BASE_MOVIE_URL).buildUpon()
+                .appendPath(POPULAR_PARAM)
+                .appendQueryParameter(API_KEY_PARAM, Consts.API_KEY)
+                .build();
+        URL url = null;
+        try{
+            url = new URL(builtUri.toString());
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return url;
+    }
 }
