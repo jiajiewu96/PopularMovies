866b2f9a976c3527b88fa0033b806bb139bb402f
wackieju <jiajie.wu96@gmail.com>
2019-08-10 15:30:50 -0700

    Added inital parameters and urls to networkutils

--- app/src/main/java/com/example/popularmovies/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/popularmovies/Utils/NetworkUtils.java
@@ -1,4 +1,16 @@
 package com.example.popularmovies.Utils;
 
 public class NetworkUtils {
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String BASE_MOVIE_URL =
+            "http://api.themoviedb.org/3/movie/";
+
+    private static final String BASE_IMAGE_URL =
+            "http://image.tmdb.org/t/p/";
+
+    private static String POPULAR_PARAM = "popular";
+    private static String TOP_RATED_PARAM = "top_rated";
+
+
 }
