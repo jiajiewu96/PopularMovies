83ff1abf2aeb4b638f637e08196245aa4830328d
jiajie <jiajie.wu96@gmail.com>
2019-08-10 17:51:59 -0700

    added image path function to json utils

--- app/src/main/java/com/example/popularmovies/Utils/JsonUtils.java
+++ app/src/main/java/com/example/popularmovies/Utils/JsonUtils.java
@@ -1,4 +1,36 @@
 package com.example.popularmovies.Utils;
 
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
+
+    private static final String JSON_ARRAY_RESULTS = "results";
+    private static final String JSON_KEY_POSTER = "poster_path";
+
+    public static List<String> getImagePath(String json){
+        if(TextUtils.isEmpty(json)){
+            return null;
+        }
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONArray results = root.getJSONArray("results");
+            List<String> posterPaths = new ArrayList<>();
+            for(int i =0; i< results.length(); i++){
+                JSONObject currentMovie = results.getJSONObject(i);
+                String posterPath = currentMovie.getString("poster_path");
+                posterPaths.add(posterPath);
+            }
+            return posterPaths;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
