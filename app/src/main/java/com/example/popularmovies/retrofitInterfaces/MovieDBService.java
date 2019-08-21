93ee61f9d52de88618235c1a3ecd0f1c8cbb3f74
jiajie <jiajie.wu96@gmail.com>
2019-08-21 01:56:46 -0700

    Added trailer call for MovieDBService

--- app/src/main/java/com/example/popularmovies/retrofitInterfaces/MovieDBService.java
+++ app/src/main/java/com/example/popularmovies/retrofitInterfaces/MovieDBService.java
@@ -1,6 +1,7 @@
 package com.example.popularmovies.retrofitInterfaces;
 
 import com.example.popularmovies.model.MovieResponse;
+import com.example.popularmovies.model.TrailerResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -10,4 +11,6 @@ import retrofit2.http.Query;
 public interface MovieDBService {
     @GET("{search_param}")
     Call<MovieResponse> getMovieResponse(@Path("search_param") String searchParam, @Query("api_key") String apiKey);
+    @GET("{id}/videos")
+    Call<TrailerResponse> getTrailerResponse(@Path("id")String movieId, @Query("api_key") String apiKey);
 }
