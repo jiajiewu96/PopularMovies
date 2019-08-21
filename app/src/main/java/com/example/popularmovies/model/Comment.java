8b01447c8db643279f5a7ea61ac7ec7db341262e
jiajie <jiajie.wu96@gmail.com>
2019-08-21 14:42:39 -0700

    Added Comment and Comment response model

---
+++ app/src/main/java/com/example/popularmovies/model/Comment.java
@@ -0,0 +1,14 @@
+package com.example.popularmovies.model;
+
+public class Comment {
+    private String author;
+    private String content;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ app/src/main/java/com/example/popularmovies/model/CommentResponse.java
@@ -0,0 +1,7 @@
+package com.example.popularmovies.model;
+
+import java.util.List;
+
+public class CommentResponse {
+    private List<Comment> results;
+}
